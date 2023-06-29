26b782f966b8d9b9e394c87f917c83dd529e3f19
DavideFarci <yank92.df@gmail.com>
2023-06-29 17:52:25 +0200

    add card and set show

--- app/Http/Controllers/Guest/ComicController.php
+++ app/Http/Controllers/Guest/ComicController.php
@@ -15,7 +15,11 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+        $comics = Comic::all(); // SELECT * FROM `pastas`
+
+        // dd($pastas);
+
+        return view('comics.index', compact('comics'));
     }
 
     /**
@@ -47,7 +51,7 @@ class ComicController extends Controller
      */
     public function show(Comic $comic)
     {
-        //
+        return view('comics.show', compact('comic'));
     }
 
     /**
